b1dc3639359058a3dc00a3018b0d3e2721b65dc8
novikovaai <137191975+novikovaai@users.noreply.github.com>
2023-11-28 17:56:44 +0300

    Сделано первое домашнее задание

---
+++ 4-navigator.js
@@ -0,0 +1,6 @@
+const addressLat = 10
+const addressLong = 10
+const positionLat = 5
+const positionLong = 5
+
+console.log(((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2) ** 0.5)
