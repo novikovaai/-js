24d8e8cb85b1e1530a0e32a23eee147d41049fb6
novikovaai <137191975+novikovaai@users.noreply.github.com>
2023-11-29 16:56:30 +0300

    Пройдено управление потоком до домашнего задания

--- Основы js/app.js
+++ Основы js/app.js
@@ -1,24 +1,12 @@
-/* Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
-К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
-Выведете в консоль:
+/* Методом prompt получите ответ пользователя на вопрос "Сколько будет 7 + или - 15?".Если ответ верен, выведите в консоли "Успех", если нет - "Вы робот!", а если он введёт "Я не робот", то тоже "Успех".*/
 
-	Boolean переменную успеете ли вы взяться за работу
-	Сколько вы за неё попросите? */
-
-const stavka = 80;
-const hours = 5;
-const days = 5;
-const week = 7;
-let zakaz = 40;
-let totalDays = 11;
-
-let workDays = totalDays - Math.floor(totalDays / week) * 2;
-let workHours = workDays * hours;
-
-
-console.log(workHours >= zakaz);
-console.log(zakaz * stavka)
-
-
-let a = 'aaa'
-console.log(a + 3)
+let q = prompt("Сколько будет 7 + или - 15?");
+switch (q) {
+	case '22':
+	case '-8':
+	case 'Я не робот':
+		console.log('Успех!')
+		break
+	default:
+		console.log('Вы робот!')
+}
