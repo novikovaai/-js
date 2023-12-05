0d8d27248bd7fb6c9e03a4b5c5b96db3e5c8963f
novikovaai <137191975+novikovaai@users.noreply.github.com>
2023-12-05 15:01:42 +0300

    Пройдены функции высшего порядка до дз

--- Основы js/app.js
+++ Основы js/app.js
@@ -1,69 +1,13 @@
-// Задача вывести в консоль строку "Я люблю JS !" из массива, проходя циклом в обратном порядке и не используя reverse
-
-const arr = ['!', 'JS', 'люблю', 'Я'];
-let newArr = []
-
-for (let i = -1; i >= -(arr.length); i--){
-    newArr.push(arr.at(i))
-}
-
-console.log(newArr.join(' '))
-
-console.log('-----------------')
-/*
-Есть выгрузка  операций пользователя
-const operations = [1000, -700, 300, -500, 10000];
-а так же  начальный баланс в 100$
-Необходимо сделать функции расчета:
-- Итогового баланса
-- Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
-- Расчета среднего расхода и среднего дохода
-*/
-
-const operations = [1000, -700, 300, -500, 10000];
-let balance = 100;
-
-function balanceLeft(balance, operations) {
-    for (let element of operations) {
-        balance += element;        
+function power(pow) {
+    return function (num) {
+        return num ** pow;
     }
-    return balance
 }
 
-function checkNegative(balance, operations) {
-    for (let element of operations) {
-        balance += element;
-        if (balance < 0) {
-            return false
-        }
-    }
-    return true
-}
+console.log(power(2)(3))
 
-function median(operations) {
-    let income = 0;
-    let incomeT = 0;
-    let outcome = 0;
-    let outcomeT = 0;
-    for (let element of operations) {    
-        switch (true) {
-            case element >= 0:
-                income += element;
-                incomeT++;
-                console.log('+', income);
-                break;
-            case element < 0:
-                outcome += element;
-                outcomeT++;   
-                console.log('-', outcome);
-                break;
-        }       
-    }    
-    return [income / incomeT, outcome / outcomeT]
-}
+console.log('----------------------')
 
-console.log(balanceLeft(balance, operations))
-console.log(checkNegative(balance, operations))
-let medianOp = median(operations)
-console.log(`Средний доход ${medianOp[0]}$`)
-console.log(`Средний расход ${-medianOp[1]}$`)
+const powArrow = pow => num => num ** pow;
+
+console.log(powArrow(2)(3))
