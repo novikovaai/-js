333eb79909fe66bfa6a7650bfe1a543fe0194de6
novikovaai <137191975+novikovaai@users.noreply.github.com>
2023-12-06 16:45:53 +0300

    Пройдены итерации в массивах до дз

--- Основы js/app.js
+++ Основы js/app.js
@@ -1,13 +1,44 @@
-function power(pow) {
-    return function (num) {
-        return num ** pow;
-    }
+/*
+Имеется массив изменения цен prices, где внутри
+1й элемент массива является ценой в момент Х,
+2й - ценой в момент Y.
+Нужно преобразовать данные в массив, где будут отображены
+только положительные изменения цен: [100, 150]
+*/
+
+const prices = [[100, 150], [300, 250], [200, 120], [100, 600]]
+
+let positiveDelta = prices.map(element => element[1] - element[0]).filter(element => element > 0)
+
+console.log(positiveDelta)
+
+console.log('-----------------------')
+
+/*
+Найти среднее значение последоватедьности
+чисел с помощью reduce
+*/
+
+const arr = [1, 4, 4, 10];
+
+let avg = arr.reduce((acc, element) => {
+    return acc + element / arr.length
+}, 0)
+
+console.log(avg)
+
+console.log('-----------------------')
+
+/*
+Написать функцию, которая возвращает true,
+если элемент есть, и false, если нет.
+*/
+
+const newArr = [2, 4, 4, 10, 20];
+
+function some(arr, element) {
+   return Boolean(arr.find(el => el === element))
 }
 
-console.log(power(2)(3))
+console.log(some(newArr, 7))
 
-console.log('----------------------')
-
-const powArrow = pow => num => num ** pow;
-
-console.log(powArrow(2)(3))
