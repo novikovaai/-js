10497c5de50f306723627e8252ba7e92b21de267
novikovaai <137191975+novikovaai@users.noreply.github.com>
2023-12-07 16:24:34 +0300

    Пройдены строки до второго упражнения

--- Основы js/app.js
+++ Основы js/app.js
@@ -1,44 +1,10 @@
-/*
-Имеется массив изменения цен prices, где внутри
-1й элемент массива является ценой в момент Х,
-2й - ценой в момент Y.
-Нужно преобразовать данные в массив, где будут отображены
-только положительные изменения цен: [100, 150]
-*/
 
-const prices = [[100, 150], [300, 250], [200, 120], [100, 600]]
+const userName = 'Вася aka Terminator Perdinator Пупкин'
+const firstName = userName.slice(0, userName.indexOf(' '))
+const lastName = userName.slice(userName.lastIndexOf(' ') + 1, userName.length)
 
-let positiveDelta = prices.map(element => element[1] - element[0]).filter(element => element > 0)
+console.log(firstName, lastName)
 
-console.log(positiveDelta)
 
-console.log('-----------------------')
-
-/*
-Найти среднее значение последоватедьности
-чисел с помощью reduce
-*/
-
-const arr = [1, 4, 4, 10];
-
-let avg = arr.reduce((acc, element) => {
-    return acc + element / arr.length
-}, 0)
-
-console.log(avg)
-
-console.log('-----------------------')
-
-/*
-Написать функцию, которая возвращает true,
-если элемент есть, и false, если нет.
-*/
-
-const newArr = [2, 4, 4, 10, 20];
-
-function some(arr, element) {
-   return Boolean(arr.find(el => el === element))
-}
-
-console.log(some(newArr, 7))
+console.log('-----------------')
 
