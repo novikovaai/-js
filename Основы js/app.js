7b8b48be312dab3d768b9fead0a1307abd6d6922
novikovaai <137191975+novikovaai@users.noreply.github.com>
2023-12-12 16:54:32 +0300

    Пройдено управление this до домашнего задания

--- Основы js/app.js
+++ Основы js/app.js
@@ -1,34 +1,63 @@
 'use strict';
-
 /*
-	Дополнить объект методом для получения имени:
-	- компании
-	- сео
-	- сотрудника
+Создайте объект пользователя с паролем.
+С помощью функции ниже удалить пароль сделав
+функцию сброса пароля
 */
 
-const company = {
-	name: "ООО Агро",
-	employees: [
-		{
-			name: "Света",
-		},
-	],
-	ceo: {
-		name: "Вася",
-
-	},
-	getCompanyName() {
-		return this.name
-	},
-	getEmployeesName() {
-		return this.employees.map(element => element.name)
-	},
-	getCeoName() {
-		return this.ceo.name
+function removePassword(reset) {
+	if (reset) {
+		this.password = undefined;
+	} else {
+		this.password = '1';
 	}
+}
+
+const user = {
+	userName: 'Вася',
+	password: 'sJDHGE@sh'
+
+}
+
+const removeUserPassword = removePassword.bind(user, true)
+
+removeUserPassword()
+
+console.log(user)
+
+console.log('--------------')
+/* Сделать функию пользователя которая берет за основу userInfo и за счет замыкания создает новый объект, с которым можно работать как user1().increase(100) */
+
+const userInfo = {
+    balance: 0,
+    operations: 0,
+    increase(sum) {
+        this.balance += sum;
+        this.operations++;
+        },
 };
 
-console.log(company.getCompanyName())
-console.log(company.getEmployeesName())
-console.log(company.getCeoName())
+function user() {
+    const userObj = {
+        balance: 0,
+        operations: 0,
+        };
+    userObj.increase = userInfo.increase
+    return function () {
+        return userObj
+        }
+}
+
+const user1 = user();
+user1().increase(100)
+user1().increase(100)
+
+console.log(userInfo.balance)
+
+const user2 = user();
+user2().increase(150)
+user2().increase(150)
+console.log(userInfo.balance)
+console.log(user1().balance)
+console.log(user2().balance)
+
