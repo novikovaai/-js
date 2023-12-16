2c3ceb43920ab2d5115839b07bdd5774c4e51721
novikovaai <137191975+novikovaai@users.noreply.github.com>
2023-12-16 16:45:13 +0300

    Пройдены уроки по DOM до первого упражнения

--- Основы js/app.js
+++ Основы js/app.js
@@ -1,63 +1,40 @@
 'use strict';
-/*
-Создайте объект пользователя с паролем.
-С помощью функции ниже удалить пароль сделав
-функцию сброса пароля
-*/
 
-function removePassword(reset) {
-	if (reset) {
-		this.password = undefined;
-	} else {
-		this.password = '1';
-	}
-}
+// document.querySelector('.button').addEventListener('click', function () { 
+//     const input = document.querySelector('.input').value;
+//     if (!input) {
+//         return;
+//     }
+//     document.querySelector('.panel').innerText = input  
+//     document.querySelector('.input').value = '' 
+//  })
 
-const user = {
-	userName: 'Вася',
-	password: 'sJDHGE@sh'
-
-}
-
-const removeUserPassword = removePassword.bind(user, true)
-
-removeUserPassword()
-
-console.log(user)
-
-console.log('--------------')
-/* Сделать функию пользователя которая берет за основу userInfo и за счет замыкания создает новый объект, с которым можно работать как user1().increase(100) */
-
-const userInfo = {
-    balance: 0,
-    operations: 0,
-    increase(sum) {
-        this.balance += sum;
-        this.operations++;
-        },
+function submitForm() {
+    const input = document.querySelector('.input').value;
+    if (!input) {
+        return;
+    }
+    document.querySelector('.panel').innerText = input;
+    document.querySelector('.input').value = '';
+    // document.querySelector('.notification').style.display = 'block';
+    document.querySelector('.notification').classList.add('notification_active');
 };
 
-function user() {
-    const userObj = {
-        balance: 0,
-        operations: 0,
-        };
-    userObj.increase = userInfo.increase
-    return function () {
-        return userObj
-        }
+function changedInput(e) {
+    if (e.code === 'Enter') {
+        submitForm()
+    }
 }
 
-const user1 = user();
-user1().increase(100)
-user1().increase(100)
 
-console.log(userInfo.balance)
+// document.querySelector('.input').addEventListener('keydown', (e) => {
+//     if (e.code === 'Enter') {
+//         submitForm()
+//     }
+// })
 
-const user2 = user();
-user2().increase(150)
-user2().increase(150)
-console.log(userInfo.balance)
-console.log(user1().balance)
-console.log(user2().balance)
+const panelText = document.querySelector('.panel').innerText;
 
+
+
+console.log(panelText)
